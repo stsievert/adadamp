3bc2b04933c1d1ab5ce58f0c66f655228ee98eb9
Scott Sievert <stsievert@users.noreply.github.com>
2020-03-02 07:57:13 -0600

    Basic example

---
+++ dist_example.py
@@ -0,0 +1,197 @@
+from __future__ import print_function
+from types import SimpleNamespace
+from typing import Callable, Dict, Any, Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+
+def gradient(
+    model: nn.Module,
+    input,
+    target,
+    loss: Callable,
+    device: torch.device = torch.device("cpu"),
+) -> Dict[str, Union[torch.Tensor, int]]:
+    r"""
+    Compute the model gradient for the function ``loss``.
+
+    Parameters
+    ----------
+    model : nn.Module
+        PyTorch model to eval
+    data : PyTorch DataSet
+    loss : Callabale
+        PyTorch loss. Should return the loss with
+
+        .. code:: python
+
+           loss(output, out_, reduction="sum")
+
+    device : PyTorch device (optional)
+
+    Returns
+    -------
+    grad : Dict[str, Union[Tensor, int]]
+        Gradients. This dictionary all keys in model.named_parameters.keys().
+
+    Notes
+    -----
+    This function computes the gradient of the *sum* of inputs, not the *mean*
+    of inputs. Functionally, this means evaluates the gradient of
+    :math:`\sum_{i=1}^B l(...)`, not :math:`\frac{1}{n} \sum_{i=1}^B l(...)`
+    where `l` is the loss function for a single example.
+
+    """
+    output = model(input)
+    _loss = loss(output, target, reduction="sum")
+    _loss.backward()
+    ret = {k: v.grad for k, v in model.named_parameters()}
+    ret["_num_data"] = len(output)
+    ret["_loss"] = _loss.item()
+    return ret
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        #  output = model(data)
+        #  loss = F.nll_loss(output, target)
+        #  loss.backward()
+        grads = gradient(model, data, target, F.nll_loss, device=device)
+        optimizer.zero_grad()
+        for name, param in model.named_parameters():
+            param.grad = grads[name]
+        loss = grads["_loss"] / grads["_num_data"]
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss,
+                )
+            )
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
+
+
+def main():
+    # Training settings
+    args = SimpleNamespace(
+        batch_size=64,
+        epochs=14,
+        gamma=0.7,
+        log_interval=10,
+        lr=1.0,
+        no_cuda=False,
+        save_model=False,
+        seed=1,
+        test_batch_size=1000,
+    )
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            "../data",
+            train=True,
+            download=True,
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+            ),
+        ),
+        batch_size=args.batch_size,
+        shuffle=True,
+        **kwargs
+    )
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            "../data",
+            train=False,
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+            ),
+        ),
+        batch_size=args.test_batch_size,
+        shuffle=True,
+        **kwargs
+    )
+
+    model = Net().to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+if __name__ == "__main__":
+    main()
