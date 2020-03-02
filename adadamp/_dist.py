BUG: average gradient

adadamp/_dist.py
@@ -4,14 +4,17 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
+
 def gradient(
     model: nn.Module,
     input,
     target,
     loss: Callable,
-    device = torch.device("cpu"),
+    device: torch.device = torch.device("cpu"),
 ) -> Dict[str, Union[torch.Tensor, int]]:
-    """
+    r"""
+    Compute the model gradient for the function ``loss``.
+
     Parameters
     ----------
     model : nn.Module
@@ -31,11 +34,18 @@ def gradient(
     grad : Dict[str, Union[Tensor, int]]
         Gradients. This dictionary all keys in model.named_parameters.keys().
 
+    Notes
+    -----
+    This function computes the gradient of the *sum* of inputs, not the *mean*
+    of inputs. Functionally, this means evaluates the gradient of
+    :math:`\sum_{i=1}^B l(...)`, not :math:`\frac{1}{n} \sum_{i=1}^B l(...)`
+    where `l` is the loss function for a single example.
+
     """
     output = model(input)
     loss = loss(output, target, reduction="sum")
     loss.backward()
     ret = {k: v.grad for k, v in model.named_parameters()}
     ret["_num_data"] = len(output)
+    ret["_loss"] = loss.item()
     return ret
-

dist_example.py
@@ -10,50 +10,7 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
 
-def gradient(
-    model: nn.Module,
-    input,
-    target,
-    loss: Callable,
-    device: torch.device = torch.device("cpu"),
-) -> Dict[str, Union[torch.Tensor, int]]:
-    r"""
-    Compute the model gradient for the function ``loss``.
-
-    Parameters
-    ----------
-    model : nn.Module
-        PyTorch model to eval
-    data : PyTorch DataSet
-    loss : Callabale
-        PyTorch loss. Should return the loss with
-
-        .. code:: python
-
-           loss(output, out_, reduction="sum")
-
-    device : PyTorch device (optional)
-
-    Returns
-    -------
-    grad : Dict[str, Union[Tensor, int]]
-        Gradients. This dictionary all keys in model.named_parameters.keys().
-
-    Notes
-    -----
-    This function computes the gradient of the *sum* of inputs, not the *mean*
-    of inputs. Functionally, this means evaluates the gradient of
-    :math:`\sum_{i=1}^B l(...)`, not :math:`\frac{1}{n} \sum_{i=1}^B l(...)`
-    where `l` is the loss function for a single example.
-
-    """
-    output = model(input)
-    _loss = loss(output, target, reduction="sum")
-    _loss.backward()
-    ret = {k: v.grad for k, v in model.named_parameters()}
-    ret["_num_data"] = len(output)
-    ret["_loss"] = _loss.item()
-    return ret
+from adadamp._dist import gradient
 
 
 class Net(nn.Module):
@@ -89,9 +46,8 @@ def train(args, model, device, train_loader, optimizer, epoch):
         #  loss = F.nll_loss(output, target)
         #  loss.backward()
         grads = gradient(model, data, target, F.nll_loss, device=device)
-        optimizer.zero_grad()
         for name, param in model.named_parameters():
-            param.grad = grads[name]
+            param.grad = grads[name] / grads["_num_data"]
         loss = grads["_loss"] / grads["_num_data"]
         optimizer.step()
         if batch_idx % args.log_interval == 0: