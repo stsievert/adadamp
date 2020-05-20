ee585a88b4297e9b478bdd4f9f6dccc3916eb036
JoeHolt <jpholt2@wisc.edu>
2020-05-20 13:52:08 -0500

    Added work on gradient function

--- adadamp/_dist.py
+++ adadamp/_dist.py
@@ -1,6 +1,8 @@
 from typing import Callable, Dict, Any, Union, Optional, List
+from torch.autograd import Variable
 
 import torch
+from copy import copy
 import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
@@ -13,7 +15,7 @@ def gradient(
     *,
     model: nn.Module,
     loss: Callable,
-    device: torch.device = torch.device("cpu"),
+    device = torch.device("cpu"),
     idx: Optional[IntArray] = None,
 ) -> Dict[str, Union[torch.Tensor, int]]:
     r"""
@@ -56,28 +58,29 @@ def gradient(
     where `l` is the loss function for a single example.
 
     """
-    # data_target = None
-    # if idx is not None:
-    #     data_target = [train_set for i in idx] 
-    # else:
-    #     data_target = [train_set for i in len(data_target)]
-
-    # inputs = [d[0].reshape(-1, *d[0].size()) for d in data_target] 
-    # target = [d[1] for d in data_target]
-
-    inputs = [pt[0] for pt in train_set] # client.scatter(train_data)
-    targets  = [pt[1] for pt in train_set]  #client.scatter(train_lbl)
+    data_target = list(train_set)
+    inputs = [d[0].reshape(-1, *d[0].size()) for d in data_target] 
+    targets = [d[1] for d in data_target]
 
     # inputs = [pt[0] for pt in train_set] # client.scatter(train_data)
     # targets  = [pt[1] for pt in train_set]  #client.scatter(train_lbl)
 
     if idx is not None:
-        # TypeError: only integer scalar arrays can be converted to a scalar index
-        inputs = inputs[idx]
-        targets = targets[idx]
-    inputs, targets = inputs.to(device), targets.to(device)
+        # HELP: Where are the extra array nests coming from?
+        inputs = [inputs[i][0][0] for i in idx]
+        targets = [targets[i] for i in idx]
+    # temporary
+    int_0 = copy(inputs[0])
+    print("Inputs - type: {} - len: {} = {}".format(type(inputs), len(inputs), inputs))
+    print("Targets - type: {} - len: {} = {}".format(type(targets), len(targets), targets))
+    # attempting to convert array of tensors into 1 tensor for inputs
+    # attempting to convert normal python list of ints into 1 tensor for targets
+    inputs, targets = torch.stack(inputs).to(device), torch.tensor(targets, device=device)
 
-    outputs = model(inputs)
+    # int_0 is of size 28x28
+    outputs = model(int_0)
+    print(outputs)
+
     loss = loss(outputs, targets, reduction="sum")
     loss.backward()
     grads = {k: v.grad for k, v in model.named_parameters()}
