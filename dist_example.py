285a228d820b52dd300705f82be35fd6bedc0efb
Scott <stsievert@users.noreply.github.com>
2020-07-21 15:31:08 -0500

    Make client optional

--- dist_example.py
+++ dist_example.py
@@ -1,22 +1,26 @@
 from __future__ import print_function
-from types import SimpleNamespace
-from typing import Callable, Dict, Any, Union, Any, Union, Optional
+
+import sys
+import time
 from copy import copy, deepcopy
+from functools import lru_cache, partial
+from types import SimpleNamespace
+from typing import Any, Callable, Dict, Optional, Union
+
+import dask
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from distributed import Client, LocalCluster
+from torch.optim.lr_scheduler import StepLR
+from torchvision import datasets, transforms
 from torchvision.datasets import FashionMNIST
 from torchvision.transforms import Compose
-from dist_example_model import Net
-import torch
-import torch.optim as optim
-import sys
-import torch.nn.functional as F
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
-import numpy as np
-import time
-from distributed import Client, LocalCluster
+
 from adadamp._dist import gradient
-from functools import partial, lru_cache
-import pandas as pd
+from dist_example_model import Net
 
 
 @lru_cache()
@@ -49,7 +53,7 @@ def _batch_size(model_updates: int, base: int = 64, increase: float = 0.1) -> in
     return int(np.ceil(base + increase * model_updates))
 
 
-def _get_gradients(client, model_future, n, idx, batch_size, n_workers):
+def _get_gradients(model_future, n, idx, batch_size, n_workers, client=None):
     """
     Calculates the gradients at a given state. This function is
     mainly in charge of sending the gradient calculation off to the
@@ -63,16 +67,12 @@ def _get_gradients(client, model_future, n, idx, batch_size, n_workers):
     Parameters
     ----------
 
-    client : distributed.Client()
-        Dask client used to scheduele workers. Assumes client
-        is backed by some cluster (ie LocalCluster or equiv)
-
     model_future : distributed.future (?)
         Future object of the model to get gradients for
 
     n : int
         number of items in training set
-    
+
     idx: int
         Current epoch, used to set up random state
 
@@ -81,6 +81,10 @@ def _get_gradients(client, model_future, n, idx, batch_size, n_workers):
 
     n_workers: int
         Number of workers current running
+
+    client : distributed.Client()
+        Dask client used to scheduele workers. Assumes client
+        is backed by some cluster (ie LocalCluster or equiv)
 
     """
     # pass a seed here
@@ -101,14 +105,10 @@ def _get_gradients(client, model_future, n, idx, batch_size, n_workers):
     #
     # Distribute the computation of the gradient. In practice this will
     # mean (say) 4 GPUs to accelerate the gradient computation. Right now
-    # for ease it's a small network that doesn't need much acceleration. 
-    grads = [
-        client.submit(
-            gradient, train_set, model=model_future, loss=F.nll_loss, idx=worker_idx,
-        )
-        for worker_idx in worker_idxs
-    ]
-    return client.gather(grads)
+    # for ease it's a small network that doesn't need much acceleration.
+    grads = [dask.delayed(gradient)(train_set, model=model_future, loss=F.nll_loss, idx=worker_idx) for worker_idx in worker_idxs]
+    return dask.compute(*grads)
+    #  return client.gather(grads)
 
 
 def test(args, model, device, test_loader, verbose=True):
@@ -137,7 +137,7 @@ def test(args, model, device, test_loader, verbose=True):
     return {"acc": acc, "loss": test_loss}
 
 
-def train_model(model, train_set, kwargs):
+def train_model(model, train_set, kwargs, client=None, cluster=None):
     """
     Trains model using an adaptive batch size
 
@@ -166,10 +166,6 @@ def train_model(model, train_set, kwargs):
     print("Creating Dask client...")
     start = time.time()
     n_workers = kwargs["initial_workers"]
-    # LocalCluster is just a stand in for now, ideally 
-    # this would be some network cluster when running the experimenets
-    cluster = LocalCluster(n_workers=n_workers)
-    client = Client(cluster)
 
     # scatter data ahead of time
     client_init = time.time() - start
@@ -178,14 +174,11 @@ def train_model(model, train_set, kwargs):
     print("Running SDG on model for {} epochs...".format(kwargs["epochs"]))
     opt = optim.SGD(model.parameters(), lr=kwargs["lr"])
 
-    # init our metric tracking
-    metrics = []  # will add pandas datafram with keys: loop_time, grad_time
-    start_time = time.time()
-
     # run gradients for however many grads
     bs = -1
     log_suffix = kwargs["log_suffix"]
     for model_updates in range(kwargs["epochs"]):
+        print(model_updates)
 
         loop_start = time.time()
 
@@ -198,67 +191,46 @@ def train_model(model, train_set, kwargs):
                 kwargs["initial_batch_size"], model_updates, kwargs["batch_growth_rate"]
             )
             n_workers = max(kwargs["initial_workers"], bs // kwargs["grads_per_worker"])
-            print(
-                "Updating batch size to {}, computing across {} workers".format(
-                    bs, n_workers
-                )
-            )
+
             # we want the works to scale with the batch size exactly
-            if cluster.workers != n_workers:
+            if cluster and cluster.workers != n_workers:
                 cluster.scale(n_workers)
+
         # use the model to get the next grad step
         new_model = copy(model)
+
         # this removes training spefic data from the model
         new_model.eval()
-        model_future = client.scatter(new_model, broadcast=True)
+        if client:
+            model_future = client.scatter(new_model, broadcast=True)
+        else:
+            model_future = new_model
+
         # compute grads
         grad_start = time.time()
         grads = _get_gradients(
-            client,
             model_future,
             n=len(train_set),
             idx=model_updates,
             batch_size=bs,
             n_workers=n_workers,
-        )  # a call to Dask
+            client=client,
+        )
         grad_time = time.time() - grad_start
+
         # update SGD
         opt.zero_grad()
         num_data = sum(info["_num_data"] for info in grads)
         assert num_data == bs
+
         # aggregate and update the gradients
         for name, param in model.named_parameters():
             grad = sum(grad[name] for grad in grads)
             # --- move the averaging to get_gradiations
             param.grad = grad / num_data
 
-        # update metrics!
-        metrics.append(
-            {
-                "grad_time": grad_time,
-                "bs": bs,
-                "n_workers": n_workers,
-                "loop_time": time.time() - loop_start,
-                "kwargs": kwargs,
-                "idx": model_updates,
-            }
-        )
-
-        if model_updates % kwargs["save_freq"] == 0:
-            print("Saving metrics to disk")
-            pd.DataFrame(metrics).to_csv("loop_metrics" + log_suffix + ".csv")
-
-    # have last entry have overall data: total time, client init time, send to data to clinet time
-    overview_metrics = pd.DataFrame.from_dict(
-        {"train_time": time.time() - start_time, "client_init": client_init}
-    )
-    loop_metrics = pd.DataFrame(metrics)
-    overview_metrics.to_csv("overview_metrics" + log_suffix + ".csv")
-    loop_metrics.to_csv("loop_metrics" + log_suffix + ".csv")
-
 
 if __name__ == "__main__":
-    # from to-joe
     kwargs = {
         "lr": 0.0433062924,
         "batch_growth_rate": 0.3486433523,
@@ -271,6 +243,11 @@ if __name__ == "__main__":
         "save_freq": 10,
         "log_suffix": "_t1",
     }
+
+    #  cluster = LocalCluster(n_workers=kwargs["initial_workers"])
+    #  client = Client(cluster)
+    cluster = client = None
+
     model = Net()
     train_set, test_set = _get_fashionmnist()
-    train_model(model, train_set, kwargs)
+    train_model(model, train_set, kwargs, client=client, cluster=cluster)
